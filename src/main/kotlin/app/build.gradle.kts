4d76bb4ce5f7fa847889beba01cbcfad64648356
Iván Pardo <123502604+Dimich1tri@users.noreply.github.com>
2025-03-29 16:19:21 -0500

    Revert "Features saviladavidp"

--- src/main/kotlin/app/build.gradle.kts
+++ src/main/kotlin/app/build.gradle.kts
@@ -56,8 +56,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.cardview)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
